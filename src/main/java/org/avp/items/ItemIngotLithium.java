0cb81147c29310522bd9521eed7b582cb41d7417
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-03 06:19:18 -0500

    Fixed a bug preventing lithium from being used in recipes.

--- src/main/java/org/avp/items/ItemIngotLithium.java
+++ src/main/java/org/avp/items/ItemIngotLithium.java
@@ -8,29 +8,42 @@ import com.arisux.amdxlib.lib.world.item.HookedItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 public class ItemIngotLithium extends HookedItem
 {
-	public boolean isDepleting = true;
-	public int depletionTicks = 20 * 60;
+    public int depletionTicks = 20 * 60;
 
-	public ItemIngotLithium()
-	{
-		this.setMaxDurability(20 * 60);
-	}
+    public ItemIngotLithium()
+    {
+        ;
+    }
 
-	@Override
-	public void onUpdate(ItemStack itemstack, World worldObj, Entity entity, int slot, boolean selected)
-	{
-		super.onUpdate(itemstack, worldObj, entity, slot, selected);
+    @Override
+    public void onUpdate(ItemStack itemstack, World worldObj, Entity entity, int slot, boolean selected)
+    {
+        super.onUpdate(itemstack, worldObj, entity, slot, selected);
 
-		this.setDamage(itemstack, itemstack.getCurrentDurability() + 1);
+        if (itemstack != null && itemstack.getTagCompound() != null)
+        {
+            int currentDepletion = itemstack.getTagCompound().getInteger("depletion");
+            NBTTagCompound newTag = new NBTTagCompound();
+            newTag.setInteger("depletion", currentDepletion + 1);
+            itemstack.setTagCompound(newTag);
+            currentDepletion = itemstack.getTagCompound().getInteger("depletion");
 
-		if (itemstack.getCurrentDurability() >= itemstack.getMaxDurability())
-		{
-			Worlds.createExplosion(entity, worldObj, new CoordData(entity), 1F, true, true, true);
-			Inventories.consumeItem((EntityPlayer) entity, this, true);
-		}
-	}
+            if (currentDepletion >= depletionTicks)
+            {
+                Worlds.createExplosion(entity, worldObj, new CoordData(entity), 1F, true, true, !worldObj.isRemote);
+                Inventories.consumeItem((EntityPlayer) entity, this, true);
+            }
+        }
+        else if (itemstack != null && itemstack.getTagCompound() == null)
+        {
+            NBTTagCompound newTag = new NBTTagCompound();
+            newTag.setInteger("depletion", 1);
+            itemstack.setTagCompound(newTag);
+        }
+    }
 }
