958e55e5f96281de6a905549bab303f0ba8b385a
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-03 10:55:32 -0500

    Fixed a bug when using pickblock that caused the mod's slabs to return stone slabs.

--- src/main/java/org/avp/block/BlockCustomSlab.java
+++ src/main/java/org/avp/block/BlockCustomSlab.java
@@ -1,7 +1,13 @@
 package org.avp.block;
 
+import com.arisux.amdxlib.lib.game.Game;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
 
 public class BlockCustomSlab extends BlockSlab
 {
@@ -34,10 +40,16 @@ public class BlockCustomSlab extends BlockSlab
     {
         return getUnlocalizedName() + "-slab";
     }
-    
+
     public BlockCustomSlab setTexture(String textureName)
     {
         this.setTextureName(textureName);
         return this;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public Item getItem(World worldIn, int x, int y, int z)
+    {
+        return Game.getItem(this);
     }
 }
