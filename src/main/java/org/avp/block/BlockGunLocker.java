cce97409d2d87c4588cd83954925807c1479472b
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-08 18:26:56 -0500

    Supply crates, gun lockers, and standard lockers now drop their contained items if they are destroyed.

--- src/main/java/org/avp/block/BlockGunLocker.java
+++ src/main/java/org/avp/block/BlockGunLocker.java
@@ -1,9 +1,12 @@
 package org.avp.block;
 
 import org.avp.entities.tile.TileEntityGunLocker;
+import org.avp.entities.tile.TileEntityLocker;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -37,5 +40,27 @@ public class BlockGunLocker extends BlockLocker
     public TileEntity createTileEntity(World world, int metadata)
     {
         return new TileEntityGunLocker();
+    }
+
+    @Override
+    public void onBlockPreDestroy(World worldIn, int x, int y, int z, int meta)
+    {
+        super.onBlockPreDestroy(worldIn, x, y, z, meta);
+        
+        TileEntityGunLocker locker = (TileEntityGunLocker) worldIn.getTileEntity(x, y, z);
+        
+        if (locker != null)
+        {
+            for (int i = locker.inventory.getSizeInventory(); i > 0; i--)
+            {
+                ItemStack stack = locker.inventory.getStackInSlot(i);
+                
+                if (stack != null)
+                {
+                    EntityItem entityItem = new EntityItem(worldIn, x, y, z, stack);
+                    worldIn.spawnEntityInWorld(entityItem);
+                }
+            }
+        }
     }
 }
--- src/main/java/org/avp/block/BlockLocker.java
+++ src/main/java/org/avp/block/BlockLocker.java
@@ -9,6 +9,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -82,6 +83,28 @@ public class BlockLocker extends Block
         {
             IRotatable rotatable = (IRotatable) tile;
             rotatable.setDirection(getFacing(placer));
+        }
+    }
+
+    @Override
+    public void onBlockPreDestroy(World worldIn, int x, int y, int z, int meta)
+    {
+        super.onBlockPreDestroy(worldIn, x, y, z, meta);
+        
+        TileEntityLocker locker = (TileEntityLocker) worldIn.getTileEntity(x, y, z);
+        
+        if (locker != null)
+        {
+            for (int i = locker.inventory.getSizeInventory(); i > 0; i--)
+            {
+                ItemStack stack = locker.inventory.getStackInSlot(i);
+                
+                if (stack != null)
+                {
+                    EntityItem entityItem = new EntityItem(worldIn, x, y, z, stack);
+                    worldIn.spawnEntityInWorld(entityItem); 
+                }
+            }
         }
     }
 
