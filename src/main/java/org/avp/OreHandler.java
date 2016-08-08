997f8ede44cfa8673971cfc9f80567825b9b0144
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-07 19:08:47 -0500

    Added more ore dictionary entries.

--- src/main/java/org/avp/OreHandler.java
+++ src/main/java/org/avp/OreHandler.java
@@ -15,14 +15,18 @@ public class OreHandler implements IInitEvent
         OreDictionary.registerOre("ingotCopper", AliensVsPredator.items().itemIngotCopper);
         OreDictionary.registerOre("oreLithium", AliensVsPredator.blocks().oreLithium);
         OreDictionary.registerOre("lithium", AliensVsPredator.items().itemIngotLithium);
+        OreDictionary.registerOre("itemLithium", AliensVsPredator.items().itemIngotLithium);
         OreDictionary.registerOre("oreAluminum", AliensVsPredator.blocks().oreBauxite);
         OreDictionary.registerOre("ingotAluminum", AliensVsPredator.items().itemIngotAluminum);
         OreDictionary.registerOre("oreAluminium", AliensVsPredator.blocks().oreBauxite);
         OreDictionary.registerOre("ingotAluminium", AliensVsPredator.items().itemIngotAluminum);
         OreDictionary.registerOre("oreSilicon", AliensVsPredator.blocks().oreSilicon);
         OreDictionary.registerOre("silicon", AliensVsPredator.items().itemSilicon);
+        OreDictionary.registerOre("itemSilicon", AliensVsPredator.items().itemSilicon);
         OreDictionary.registerOre("polycarbonate", AliensVsPredator.items().itemPolycarbonate);
+        OreDictionary.registerOre("itemPolycarbonate", AliensVsPredator.items().itemPolycarbonate);
         OreDictionary.registerOre("plastic", AliensVsPredator.items().itemPolycarbonate);
+        OreDictionary.registerOre("itemPlastic", AliensVsPredator.items().itemPolycarbonate);
         OreDictionary.registerOre("logWood", AliensVsPredator.blocks().terrainUniTreeLog);
     }
 }
