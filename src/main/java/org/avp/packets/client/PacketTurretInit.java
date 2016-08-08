95d1a319e6c054f2e7bd0cdc5b2ed9afd8265f95
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-08 10:40:10 -0500

    Fixed a bug with turrets causing a server crash.

--- src/main/java/org/avp/packets/client/PacketTurretInit.java
+++ src/main/java/org/avp/packets/client/PacketTurretInit.java
@@ -9,6 +9,7 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 
 public class PacketTurretInit implements IMessage, IMessageHandler<PacketTurretInit, PacketTurretInit>
@@ -63,7 +64,7 @@ public class PacketTurretInit implements IMessage, IMessageHandler<PacketTurretI
             {
                 if (!id.equals(""))
                 {
-                    tile.setDangerous(EntityList.getClassFromID(Integer.valueOf(id)));
+                    tile.setDangerous((Class<? extends Entity>) EntityList.stringToClassMapping.get(String.valueOf(id)));
                 }
             }
         }
