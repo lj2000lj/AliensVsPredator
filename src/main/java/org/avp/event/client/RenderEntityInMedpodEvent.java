7e532abde53cb56ebfa4ecbf773fac07f1ebbfd1
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-09-25 02:58:07 -0500

    Permanent fix for the medpod rendering issues.

--- src/main/java/org/avp/event/client/RenderEntityInMedpodEvent.java
+++ src/main/java/org/avp/event/client/RenderEntityInMedpodEvent.java
@@ -1,7 +1,8 @@
 package org.avp.event.client;
 
 import org.avp.entities.EntityMedpod;
-import org.avp.entities.extended.ExtendedEntityPlayer;
+import org.avp.entities.mob.EntityMarine;
+import org.avp.entities.mob.EntitySpeciesYautja;
 import org.lwjgl.opengl.GL11;
 
 import com.arisux.amdxlib.lib.client.Model;
@@ -19,6 +20,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderLivingEvent;
@@ -31,57 +33,19 @@ public class RenderEntityInMedpodEvent
     @SubscribeEvent
     public void renderPlayerTickPre(RenderPlayerEvent.Pre event)
     {
-        if (event.entity != null)
-        {
-            if (event.entity instanceof AbstractClientPlayer)
-            {
-                ExtendedEntityPlayer extendedPlayer = (ExtendedEntityPlayer) event.entity.getExtendedProperties(ExtendedEntityPlayer.IDENTIFIER);
-
-                if (extendedPlayer != null && extendedPlayer.getPlayer() != null && extendedPlayer.getPlayer().ridingEntity instanceof EntityMedpod)
-                {
-                    event.setCanceled(true);
-
-                    EntityPlayer client = Game.minecraft().thePlayer;
-
-                    double renderX = event.entity.lastTickPosX + (event.entity.posX - event.entity.lastTickPosX) * (double) event.partialRenderTick;
-                    double renderY = event.entity.lastTickPosY + (event.entity.posY - event.entity.lastTickPosY) * (double) event.partialRenderTick;
-                    double renderZ = event.entity.lastTickPosZ + (event.entity.posZ - event.entity.lastTickPosZ) * (double) event.partialRenderTick;
-
-                    double clientX = client.lastTickPosX + (client.posX - client.lastTickPosX) * (double) event.partialRenderTick;
-                    double clientY = client.lastTickPosY + (client.posY - client.lastTickPosY) * (double) event.partialRenderTick;
-                    double clientZ = client.lastTickPosZ + (client.posZ - client.lastTickPosZ) * (double) event.partialRenderTick;
-
-                    renderX = renderX - clientX;
-                    renderY = renderY - clientY;
-                    renderZ = renderZ - clientZ;
-
-                    renderLiving.render((EntityLivingBase) event.entity, event.renderer, renderX, renderY, renderZ, event.partialRenderTick);
-                }
-            }
-        }
+        ;
     }
 
     @SubscribeEvent
     public void renderPlayerTickPost(RenderPlayerEvent.Post event)
     {
-        if (event.entity != null)
-        {
-            if (event.entity instanceof AbstractClientPlayer)
-            {
-                ExtendedEntityPlayer extendedPlayer = (ExtendedEntityPlayer) event.entity.getExtendedProperties(ExtendedEntityPlayer.IDENTIFIER);
-
-                if (extendedPlayer != null && extendedPlayer.getPlayer() != null && extendedPlayer.getPlayer().ridingEntity instanceof EntityMedpod)
-                {
-                    // event.setCanceled(true);
-                }
-            }
-        }
+        ;
     }
 
     @SubscribeEvent
     public void entityRenderEventPre(RenderLivingEvent.Pre event)
     {
-        if (event.entity != null && event.entity.ridingEntity instanceof EntityMedpod && !(event.entity instanceof EntityPlayer))
+        if (event.entity != null && event.entity.ridingEntity instanceof EntityMedpod)
         {
             event.setCanceled(true);
             renderLiving.render(event.entity, event.renderer, event.x, event.y, event.z, Game.partialTicks());
@@ -91,7 +55,7 @@ public class RenderEntityInMedpodEvent
     @SubscribeEvent
     public void entityRenderEventPost(RenderLivingEvent.Post event)
     {
-        if (event.entity != null && event.entity.ridingEntity instanceof EntityMedpod && !(event.entity instanceof EntityPlayer))
+        if (event.entity != null && event.entity.ridingEntity instanceof EntityMedpod)
         {
             event.setCanceled(true);
         }
@@ -99,34 +63,27 @@ public class RenderEntityInMedpodEvent
 
     public static void transformMedpodEntity(EntityMedpod medpod, Entity entity)
     {
-        if (entity instanceof EntityOtherPlayerMP || entity instanceof EntityPlayerSP)
-        {
-            OpenGL.rotate(180F, 1F, 0F, 0F);
-            OpenGL.translate(0F, -2.5F, 0F);
-
-            if (entity instanceof EntityPlayerSP)
-            {
-                OpenGL.translate(0F, -1.45F, 0F);
-            }
-        }
-
         float medpodRotation = (float) medpod.getTileEntity().getDoorProgress() * 45 / medpod.getTileEntity().getMaxDoorProgress();
+        OpenGL.translate(0F, 0F, 0.225F);
         OpenGL.translate(0F, 1.5F, -0F);
         OpenGL.rotate(medpodRotation, 1F, 0F, 0F);
         OpenGL.translate(0F, -1.75F + entity.height, 0F);
         OpenGL.translate(0F, -0.5F, 0F);
         OpenGL.rotate(180F, 0F, 1F, 0);
-
-        if (entity instanceof EntityOtherPlayerMP || entity instanceof EntityPlayerSP)
+        
+        //Entity specific transforms
+        
+        if (entity instanceof EntityVillager || entity instanceof EntityMarine || entity instanceof EntitySpeciesYautja)
         {
-            OpenGL.rotate(90F, 1F, 0F, 0F);
-            OpenGL.rotate(180F, 0F, 0F, 1F);
-            OpenGL.translate(0F, -0.6F, -0.85F);
-
-            if (entity instanceof EntityPlayerSP)
-            {
-                OpenGL.translate(0F, 0F, 0.15F);
-            }
+            OpenGL.rotate(90F, 1F, 0F, 0);
+        }
+        
+        if (entity instanceof EntityPlayer)
+        {
+            OpenGL.translate(0F, -0.5F, 0F);
+            OpenGL.rotate(90F, 1F, 0F, 0);
+            OpenGL.rotate(180F, 0F, 1F, 0);
+            OpenGL.translate(0F, -0.75F, 0F);
         }
     }
 
@@ -192,7 +149,15 @@ public class RenderEntityInMedpodEvent
 
         protected void renderModel(EntityLivingBase living, float swingProgress, float swingProgressPrev, float idleProgress, float rotationYawHead, float rotationPitch, float boxTranslation)
         {
-            Draw.bindTexture(Entities.getEntityTexture(this.renderer, living));
+            if (living instanceof AbstractClientPlayer)
+            {
+                AbstractClientPlayer clientPlayer = (AbstractClientPlayer) living;
+                Draw.bindTexture(clientPlayer.getLocationSkin());
+            }
+            else
+            {
+                Draw.bindTexture(Entities.getEntityTexture(this.renderer, living));
+            }
 
             if (!living.isInvisible())
             {
