7c00542598beb0dd77edd4ae7fa663f8cc8cc525
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-10-26 03:29:11 -0500

    Added hostile AI similar to zombies to the Goo Mutant

--- src/main/java/org/avp/entities/mob/EntityGooMutant.java
+++ src/main/java/org/avp/entities/mob/EntityGooMutant.java
@@ -1,8 +1,19 @@
 package org.avp.entities.mob;
 
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public class EntityGooMutant extends EntityMob implements IMob
@@ -11,6 +22,18 @@ public class EntityGooMutant extends EntityMob implements IMob
     {
         super(world);
         this.experienceValue = 150;
+        this.getNavigator().setBreakDoors(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityAnimal.class, 1.0D, false));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityMob.class, 1.0D, false));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityAnimal.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityMob.class, 0, true));
     }
 
     @Override
@@ -20,6 +43,7 @@ public class EntityGooMutant extends EntityMob implements IMob
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(60.0D);
         this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.5199999761581421D);
         this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(2.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(0.3D);
     }
 
     @Override
