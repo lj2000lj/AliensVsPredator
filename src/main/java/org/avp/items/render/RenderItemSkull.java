ae4867360b2b13046f5983acdad03685bb25ab64
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-10-29 15:14:09 -0500

    Skulls now render on the ground

--- src/main/java/org/avp/items/render/RenderItemSkull.java
+++ src/main/java/org/avp/items/render/RenderItemSkull.java
@@ -6,6 +6,7 @@ import org.lwjgl.opengl.GL11;
 import com.arisux.amdxlib.lib.client.Model;
 import com.arisux.amdxlib.lib.client.render.ItemRenderer;
 import com.arisux.amdxlib.lib.client.render.OpenGL;
+import com.arisux.amdxlib.lib.game.Game;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.model.ModelRenderer;
@@ -58,6 +59,23 @@ public class RenderItemSkull extends ItemRenderer
             OpenGL.translate(8F, 1F, -16F);
             OpenGL.rotate(0F, 1.0F, 0.0F, 0.0F);
             OpenGL.scale(glScale, glScale, glScale);
+            OpenGL.enableLight();
+            this.draw((BlockSkull) Block.getBlockFromItem(item.getItem()));
+            OpenGL.disableLight();
+        }
+        OpenGL.popMatrix();
+    }
+    
+    @Override
+    public void renderInWorld(ItemStack item, Object... data)
+    {
+        OpenGL.pushMatrix();
+        {
+            float glScale = 1F;
+            OpenGL.disable(GL11.GL_CULL_FACE);
+//            OpenGL.translate(8F, 1F, -16F);
+            OpenGL.rotate((this.mc.theWorld.getWorldTime() + Game.partialTicks() % 360) * 10, 0.0F, 1.0F, 0.0F);
+            OpenGL.scale(glScale, -glScale, glScale);
             OpenGL.enableLight();
             this.draw((BlockSkull) Block.getBlockFromItem(item.getItem()));
             OpenGL.disableLight();
