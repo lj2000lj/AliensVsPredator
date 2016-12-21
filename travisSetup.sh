fc302cff326b2f39f9abf00da625e3d97a4002f9
Ri5ux <Ri5ux@users.noreply.github.com>
2016-12-20 23:46:26 -0600

    Added travisSetup.sh in attempt to fix travis (still)

---
+++ travisSetup.sh
@@ -0,0 +1,11 @@
+GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
+export GRADLE_PROPERTIES
+echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+if [ ! -f "$GRADLE_PROPERTIES" ]; then
+    echo "Gradle Properties does not exist"
+    echo "Creating Gradle Properties file..."
+    touch $GRADLE_PROPERTIES
+    echo "Writing curseForgeApiKey to gradle.properties..."
+    echo "curseForgeApiKey=00000-00000-00000-00000-00000" >> $GRADLE_PROPERTIES
+fi
