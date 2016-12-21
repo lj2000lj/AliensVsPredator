b59b839739e81acccd35de90e43ed7ba53fc2e4d
Ri5ux <Ri5ux@users.noreply.github.com>
2016-12-21 00:05:30 -0600

    This should fix travis forever.

--- travisSetup.sh
+++ travisSetup.sh
@@ -8,4 +8,6 @@ if [ ! -f "$GRADLE_PROPERTIES" ]; then
     touch $GRADLE_PROPERTIES
     echo "Writing curseForgeApiKey to gradle.properties..."
     echo "curseForgeApiKey=00000-00000-00000-00000-00000" >> $GRADLE_PROPERTIES
+    echo "mavenUsername=null" >> $GRADLE_PROPERTIES
+    echo "mavenPassword=null" >> $GRADLE_PROPERTIES
 fi
