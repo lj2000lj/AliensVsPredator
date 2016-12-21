9a38d12d373b134209ddbe6722590cf905654973
Ri5ux <Ri5ux@users.noreply.github.com>
2016-12-20 21:24:32 -0600

    MK50 spacesuit helmet should have support for warpdrive's air canisters.

--- src/main/java/org/avp/items/ItemArmorMK50.java
+++ src/main/java/org/avp/items/ItemArmorMK50.java
@@ -4,13 +4,17 @@ import org.avp.AliensVsPredator;
 
 import com.arisux.mdxlib.lib.client.render.Draw;
 
+import cr0s.warpdrive.api.IAirCanister;
+import cr0s.warpdrive.api.IBreathingHelmet;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class ItemArmorMK50 extends ItemArmor
+public class ItemArmorMK50 extends ItemArmor implements IBreathingHelmet
 {
     public ItemArmorMK50(ArmorMaterial material, int renderIndex, int armorType)
     {
@@ -39,5 +43,68 @@ public class ItemArmorMK50 extends ItemArmor
     public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
     {
         ;
+    }
+
+    @Override
+    public boolean canBreath(Entity player)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean removeAir(Entity player)
+    {
+        if (player instanceof EntityPlayerMP)
+        {
+            EntityPlayerMP playerMp = (EntityPlayerMP) player;
+            ItemStack[] inventory = playerMp.inventory.mainInventory;
+
+            for (int id = 0; id < inventory.length; id++)
+            {
+                ItemStack stack = inventory[id];
+
+                if (stack != null && stack.getItem() instanceof IAirCanister)
+                {
+                    IAirCanister airCanister = (IAirCanister) stack.getItem();
+
+                    if (airCanister.containsAir(stack))
+                    {
+                        if (stack.stackSize > 1)
+                        {
+                            stack.stackSize--;
+                            ItemStack toAdd = stack.copy();
+                            toAdd.stackSize = 1;
+                            toAdd.damageItem(1, playerMp);
+                            
+                            if (stack.getCurrentDurability() >= stack.getMaxDurability())
+                            {
+                                toAdd = airCanister.emptyDrop(stack);
+                            }
+                            if (!playerMp.inventory.addItemStackToInventory(toAdd))
+                            {
+                                EntityItem ie = new EntityItem(playerMp.worldObj, playerMp.posX, playerMp.posY, playerMp.posZ, toAdd);
+                                playerMp.worldObj.spawnEntityInWorld(ie);
+                            }
+                        }
+                        else
+                        {
+                            stack.damageItem(1, playerMp);
+                            if (stack.getCurrentDurability() >= stack.getMaxDurability())
+                            {
+                                inventory[id] = airCanister.emptyDrop(stack);
+                            }
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int ticksPerCanDamage()
+    {
+        return 40;
     }
 }
