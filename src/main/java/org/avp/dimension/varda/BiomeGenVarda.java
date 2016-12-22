8b9af4b02ae17a0b5d4d0ee5813396b8da39678f
Ri5ux <Ri5ux@users.noreply.github.com>
2016-12-21 19:06:43 -0600

    Adjusted the spawn group sizes for deacons, engineers and trilobites.

--- src/main/java/org/avp/dimension/varda/BiomeGenVarda.java
+++ src/main/java/org/avp/dimension/varda/BiomeGenVarda.java
@@ -37,6 +37,9 @@ public class BiomeGenVarda extends BiomeGenLV
         this.spawnableWaterCreatureList.clear();
         this.spawnableWaterCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityDeaconShark.class, 10, 1, 1));
         this.spawnableCaveCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityHammerpede.class, 10, 1, 3));
+        this.spawnableCaveCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityDeacon.class, 1, 0, 1));
+        this.spawnableCaveCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityEngineer.class, 1, 0, 1));
+        this.spawnableCaveCreatureList.add(new BiomeGenBase.SpawnListEntry(EntityTrilobite.class, 1, 0, 1));
     }
 
     @Override
