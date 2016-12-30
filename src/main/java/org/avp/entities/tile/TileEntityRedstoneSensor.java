13d40aaf111b7b3e26db089897e73cf624293c13
Ri5ux <Ri5ux@users.noreply.github.com>
2016-12-30 03:04:11 -0600

    Fixed a bug causing redstone sensors to only detect redstone dust.

--- src/main/java/org/avp/entities/tile/TileEntityRedstoneSensor.java
+++ src/main/java/org/avp/entities/tile/TileEntityRedstoneSensor.java
@@ -2,7 +2,9 @@ package org.avp.entities.tile;
 
 import org.avp.util.IVoltageProvider;
 
+import net.minecraft.block.BlockRedstoneComparator;
 import net.minecraft.block.BlockRedstoneWire;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -58,28 +60,28 @@ public class TileEntityRedstoneSensor extends TileEntityElectrical implements IV
         int x = this.xCoord;
         int y = this.yCoord;
         int z = this.zCoord;
-        
-        if (world.getBlock(x + 1, y, z) instanceof BlockRedstoneWire)
+
+        if (world.getBlock(x + 1, y, z).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x + 1, y, z) != 0;
         }
-        else if (world.getBlock(x, y + 1, z) instanceof BlockRedstoneWire)
+        else if (world.getBlock(x, y + 1, z).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x, y + 1, z) != 0;
         }
-        else if (world.getBlock(x, y, z + 1) instanceof BlockRedstoneWire)
+        else if (world.getBlock(x, y, z + 1).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x, y, z + 1) != 0;
         }
-        else if (world.getBlock(x - 1, y, z) instanceof BlockRedstoneWire)
+        else if (world.getBlock(x - 1, y, z).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x - 1, y, z) != 0;
         }
-        else if (world.getBlock(x, y - 1, z) instanceof BlockRedstoneWire)
+        else if (world.getBlock(x, y - 1, z).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x, y - 1, z) != 0;
         }
-        else if (world.getBlock(x, y, z - 1) instanceof BlockRedstoneWire)
+        else if (world.getBlock(x, y, z - 1).getMaterial() == Material.circuits)
         {
             this.isActiveRedstoneWireAttached = world.getBlockMetadata(x, y, z - 1) != 0;
         }
