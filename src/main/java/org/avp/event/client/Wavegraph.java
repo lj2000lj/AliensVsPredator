5f622f71a68fa8f07001613c74908b43acf4457b
Ri5ux <Ri5ux@users.noreply.github.com>
2017-01-08 02:55:18 -0600

    Fixed a compilation error.

--- src/main/java/org/avp/event/client/Wavegraph.java
+++ src/main/java/org/avp/event/client/Wavegraph.java
@@ -3,8 +3,8 @@ package org.avp.event.client;
 import java.util.ArrayList;
 import java.util.Random;
 
-import org.avp.event.client.Wavegraph.DataEntry.DisplayData;
-import org.avp.event.client.Wavegraph.DataEntry.Interval;
+import org.avp.event.client.DataEntry.DisplayData;
+import org.avp.event.client.DataEntry.Interval;
 
 import com.arisux.mdxlib.lib.client.render.Draw;
 import com.arisux.mdxlib.lib.client.render.OpenGL;
@@ -34,127 +34,6 @@ public class Wavegraph
     int                            flatlineHeight;
     int                            flatlineY;
     float                          lineWidth;
-
-    public static class DataEntry
-    {
-        protected Wavegraph   graph;
-        protected DisplayData displaydata;
-        public long           time;
-        private Interval      interval;
-
-        public DataEntry(Wavegraph graph, Interval interval)
-        {
-            this(graph);
-            this.interval = interval;
-        }
-
-        public DataEntry(Wavegraph graph)
-        {
-            this.graph = graph;
-            this.time = System.currentTimeMillis();
-            this.displaydata = new DisplayData(graph, this);
-        }
-
-        public int lengthMillis()
-        {
-            return (int) (interval.timeEnd - interval.timeStart);
-        }
-
-        public static class Segment
-        {
-            public long timeStart;
-            public long timeEnd;
-
-            public Segment(long timeStart, long timeEnd)
-            {
-                this.timeStart = timeStart;
-                this.timeEnd = timeEnd;
-            }
-        }
-
-        public static class Interval
-        {
-            public long  timeStart;
-            public long  timeEnd;
-            public float startValue;
-            public float peakValue;
-            public float endValue;
-
-            public Interval(long timeStart, long timeEnd, float startValue, float peakValue, float endValue)
-            {
-                this.timeStart = timeStart;
-                this.timeEnd = timeEnd;
-                this.startValue = startValue;
-                this.peakValue = peakValue;
-                this.endValue = endValue;
-            }
-        }
-
-        public static class DisplayData
-        {
-            protected Wavegraph e;
-            protected DataEntry r;
-
-            public int          recordTime;
-            public int          recordElapseTime;
-            public int          recordWidth;
-            public int          recordHeight;
-            public int          recordX;
-            public int          recordY;
-
-            public int          iTime;
-            public int          iWidth;
-            public int          iX;
-            public int          iEndX;
-            public int          iPeakX;
-            public int          iY;
-            public int          iPeakY;
-            public int          iEndY;
-
-            public DisplayData(Wavegraph e, DataEntry r)
-            {
-                this.e = e;
-                this.r = r;
-            }
-
-            public void update(long curTime, int x, int y, int width, int height, int widthScale, int heightScale)
-            {
-                if (this.r == null)
-                {
-                    return;
-                }
-
-                recordTime = (int) (curTime - r.time) / 16;
-                recordElapseTime = this.r.lengthMillis();
-                recordWidth = recordElapseTime / widthScale;
-                recordHeight = height / heightScale;
-                recordX = x + (width - recordTime) + recordWidth;
-                recordY = y;
-
-                if (this.r.interval != null)
-                {
-                    iTime = (int) (((this.r.interval.timeEnd - this.r.interval.timeStart)));
-                    iWidth = iTime / widthScale;
-                    iX = recordX;
-                    iEndX = iX + iWidth;
-                    iPeakX = iEndX - (iWidth / 2);
-                    iY = this.e.flatlineY - (int) Math.floor((height / 2) * this.r.interval.startValue);
-                    iPeakY = this.e.flatlineY - (int) Math.floor((height / 2) * this.r.interval.peakValue);
-                    iEndY = this.e.flatlineY - (int) Math.floor((height / 2) * this.r.interval.endValue);
-                }
-            }
-        }
-
-        public DisplayData displaydata()
-        {
-            return displaydata;
-        }
-
-        public Wavegraph getWavegraph()
-        {
-            return graph;
-        }
-    }
 
     public Wavegraph()
     {
