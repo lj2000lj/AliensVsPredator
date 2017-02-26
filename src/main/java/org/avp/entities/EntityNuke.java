f29dcc1d801cf2aebf92d158477d4bbef0e727d0
Dustin Christensen <Ri5ux@users.noreply.github.com>
2017-02-26 10:56:32 -0600

    Adjustments to the wristbracer self destruct explosion and animation.

--- src/main/java/org/avp/entities/EntityNuke.java
+++ src/main/java/org/avp/entities/EntityNuke.java
@@ -1,24 +1,37 @@
 package org.avp.entities;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.avp.AliensVsPredator;
 import org.avp.Sounds;
+import org.avp.entities.fx.EntityFXElectricArc;
 
-import com.arisux.mdxlib.lib.client.Sound;
-import com.arisux.mdxlib.lib.world.Worlds;
+import com.arisux.mdxlib.lib.game.Game;
+import com.arisux.mdxlib.lib.world.LargeExplosion;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 public class EntityNuke extends EntityThrowable
 {
+    private int preInitTicks;
+    private int initTicks;
+    private int postInitTicks;
+
     public EntityNuke(World world)
     {
         super(world);
         this.setSize(0.5F, 0.5F);
         this.yOffset = this.height / 2.0F;
         this.ignoreFrustumCheck = true;
+        this.preInitTicks = 1;
+        this.initTicks = 1;
+        this.postInitTicks = 1;
     }
 
     @Override
@@ -33,6 +46,22 @@ public class EntityNuke extends EntityThrowable
         this.motionY *= 0.9800000190734863D;
         this.motionZ *= 0.9800000190734863D;
 
+        if (this.preInitTicks < this.getPreInitTicksMax())
+        {
+            this.preInitTicks++;
+        }
+        else
+        {
+            if (this.initTicks <= this.getInitTicksMax())
+            {
+                this.initTicks++;
+            }
+            else
+            {
+                this.postInitTicks++;
+            }
+        }
+
         if (this.onGround)
         {
             this.motionX *= 0.699999988079071D;
@@ -45,24 +74,79 @@ public class EntityNuke extends EntityThrowable
             Sounds.fxWristbracerAlarm.playSound(this, 15F, 1F);
         }
 
-        if (this.ticksExisted >= this.getDetonationTicks())
+        if (this.preInitTicks >= this.getPreInitTicksMax())
         {
-            if (AliensVsPredator.settings().areExplosionsEnabled())
+            float explosionWidthMax = 80F;
+            float explosionHeightMax = explosionWidthMax / 2;
+            float explosionWidth = (float) (this.getInitTicks() * explosionWidthMax / this.getInitTicksMax());
+
+            float iS = 1F;
+            double arcFluctuation = 1 + (this.getInitTicks() * 40 / this.getInitTicksMax());
+            double arcComplexity = (explosionWidth * 0.5 / explosionWidthMax);
+            float arcDensity = (float) (0.01F) + (explosionWidth * 1F / explosionWidthMax);
+
             {
-                Worlds.createCustomExplosion((Entity) null, worldObj, (int) this.posX, (int) this.posY, (int) this.posZ, 47F);
+                double pX = this.posX + (this.rand.nextDouble() * explosionWidth) - (this.rand.nextDouble() * explosionWidth);
+                double pY = this.posY + (this.rand.nextDouble() * explosionWidth) - (this.rand.nextDouble() * explosionWidth);
+                double pZ = this.posZ + (this.rand.nextDouble() * explosionWidth) - (this.rand.nextDouble() * explosionWidth);
+
+                double sX = this.posX + (this.rand.nextDouble() * iS) - (this.rand.nextDouble() * iS);
+                double sY = this.posY + (this.rand.nextDouble() * iS) - (this.rand.nextDouble() * iS);
+                double sZ = this.posZ + (this.rand.nextDouble() * iS) - (this.rand.nextDouble() * iS);
+
+                if (this.worldObj.isRemote)
+                {
+                    Game.minecraft().effectRenderer.addEffect(new EntityFXElectricArc(this.worldObj, sX, sY, sZ, pX, pY, pZ, 10, arcFluctuation, arcComplexity, arcDensity, 0xFF4455AA));
+                }
+
+                this.worldObj.setBlockToAir((int) Math.round(pX), (int) Math.round(pY), (int) Math.round(pZ));
             }
 
-            this.setDead();
-        }
-        else
-        {
-            this.worldObj.spawnParticle("smoke", this.posX, this.posY + 0.5D, this.posZ, 0.0D, 0.0D, 0.0D);
+            if (this.getPostInitTicks() >= this.getPostInitTicksMax() * 2)
+            {
+                if (AliensVsPredator.settings().areExplosionsEnabled())
+                {
+                    ArrayList<Block> excludedBlocks = new ArrayList<Block>();
+                    excludedBlocks.add(Blocks.bedrock);
+                    ArrayList<Material> excludedMaterials = new ArrayList<Material>();
+                    excludedMaterials.add(Material.rock);
+                    LargeExplosion explosion = new LargeExplosion(worldObj, explosionWidthMax, explosionHeightMax, explosionWidthMax, (int) this.posX, (int) this.posY, (int) this.posZ, 1000F, new Random().nextLong(), excludedBlocks, excludedMaterials, 0, 2);
+                    explosion.start();
+                }
+
+                this.setDead();
+            }
         }
     }
 
-    public double getDetonationTicks()
+    public int getInitTicksMax()
     {
         return 20 * 30;
+    }
+
+    public int getInitTicks()
+    {
+        return initTicks;
+    }
+
+    public int getPreInitTicksMax()
+    {
+        return 20 * 15;
+    }
+
+    public int getPreInitTicks()
+    {
+        return preInitTicks;
+    }
+
+    public int getPostInitTicksMax()
+    {
+        return 10;
+    }
+
+    public int getPostInitTicks()
+    {
+        return postInitTicks;
     }
 
     @Override
--- src/main/java/org/avp/entities/render/RenderNuke.java
+++ src/main/java/org/avp/entities/render/RenderNuke.java
@@ -1,5 +1,6 @@
 package org.avp.entities.render;
 
+import org.avp.AliensVsPredator;
 import org.avp.entities.EntityNuke;
 import org.avp.entities.model.ModelPlasma;
 
@@ -13,54 +14,65 @@ import net.minecraft.util.ResourceLocation;
 public class RenderNuke extends Render
 {
     private ModelPlasma model = new ModelPlasma();
-    
+    private float       rotation;
+    private float       rotationPrev;
+    private float       scale;
+    private float       scalePrev;
+
     public RenderNuke()
     {
         this.model.setColor(new Color(0.3F, 0.6F, 1F, 0.7F));
     }
 
     @Override
-    public void doRender(Entity entity, double posX, double posY, double posZ, float yaw, float renderPartialTicks)
+    public void doRender(Entity entity, double posX, double posY, double posZ, float yaw, float partialTicks)
     {
         EntityNuke nuke = (EntityNuke) entity;
-        float rotation = nuke.ticksExisted % 360;
-        float scale = 0.3F * nuke.ticksExisted;
+        rotationPrev = rotation;
+        rotation = nuke.ticksExisted % 360;
+        rotation = rotationPrev + (rotation - rotationPrev) * partialTicks;
+        this.model.setColor(new Color(0.5F, 0.6F, 1F, 0.4F));
 
-        if (nuke.ticksExisted > nuke.getDetonationTicks() - (nuke.getDetonationTicks() / 25))
-        {
-            scale = (float) ((float) (0.3F * (nuke.getDetonationTicks() - nuke.ticksExisted) * 2048) / (0.2F * (nuke.getDetonationTicks())));
-        }
+        scalePrev = scale;
+        scale = nuke.getPostInitTicks() * 200 / nuke.getPostInitTicksMax();
+        scale = scalePrev + (scale - scalePrev) * partialTicks;
 
         OpenGL.pushMatrix();
         {
             OpenGL.translate(posX, posY, posZ);
+            OpenGL.rotate(180F, 1F, 0F, 0F);
+            AliensVsPredator.resources().models().WRISTBLADES.draw();
             OpenGL.rotate(entity.rotationYaw - 90.0F, 0.0F, 1.0F, 0.0F);
             OpenGL.rotate(entity.rotationPitch - 90.0F, 0.0F, 0.0F, 1.0F);
             OpenGL.scale(scale, scale, scale);
 
-            OpenGL.pushMatrix();
+            if (nuke.getPostInitTicks() > 1)
             {
-                OpenGL.rotate(rotation, 0.0F, 1.0F, 0.0F);
-                this.model.setScale(0.1F);
-                this.model.render();
-
                 OpenGL.pushMatrix();
                 {
                     OpenGL.rotate(rotation, 0.0F, 1.0F, 0.0F);
-                    this.model.setScale(0.2F);
+                    this.model.setScale(0.1F);
                     this.model.render();
 
                     OpenGL.pushMatrix();
                     {
-                        OpenGL.rotate(rotation, 0.0F, 1.0F, 0.0F);
-                        this.model.setScale(0.3F);
+                        OpenGL.rotate(rotation * 1.5F, 0.0F, 1.0F, 0.0F);
+                        this.model.setScale(0.2F);
                         this.model.render();
 
                         OpenGL.pushMatrix();
                         {
-                            OpenGL.rotate(rotation, 0.0F, 1.0F, 0.0F);
-                            this.model.setScale(0.35F);
+                            OpenGL.rotate(rotation * 1.5F, 0.0F, 1.0F, 0.0F);
+                            this.model.setScale(0.3F);
                             this.model.render();
+
+                            OpenGL.pushMatrix();
+                            {
+                                OpenGL.rotate(rotation * 1.5F, 0.0F, 1.0F, 0.0F);
+                                this.model.setScale(0.35F);
+                                this.model.render();
+                            }
+                            OpenGL.popMatrix();
                         }
                         OpenGL.popMatrix();
                     }
@@ -68,7 +80,6 @@ public class RenderNuke extends Render
                 }
                 OpenGL.popMatrix();
             }
-            OpenGL.popMatrix();
         }
         OpenGL.popMatrix();
     }
